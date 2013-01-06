813c6c9c56682ffd1d7b313ba156e6f45de88db8
Sven Arnold <sven@internetallee.de>
2013-01-06 22:39:11 +0100

    installed spring-security-core plugin

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -89,3 +89,8 @@ log4j = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+// Added by the Spring Security Core plugin:
+grails.plugins.springsecurity.userLookup.userDomainClassName = 'de.internetallee.sven.sslmonitor.User'
+grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'de.internetallee.sven.sslmonitor.UserRole'
+grails.plugins.springsecurity.authority.className = 'de.internetallee.sven.sslmonitor.Role'
